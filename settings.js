fecbbac7f82b7144c413630112a4176f56a29eef
huynt74 <nguyenthaihuy2802@gmail.com>
2024-04-08 09:55:52 +0700

    up seting

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
 // export const API_BASE_URL = 'http://localhost:5000'
-export const API_BASE_URL = process.env.API_BASE_URL
-// export const API_BASE_URL = 'https://myapp-capstone-647518bcad02.herokuapp.com'
+// export const API_BASE_URL = process.env.API_BASE_URL
+export const API_BASE_URL = 'https://myapp-capstone-647518bcad02.herokuapp.com'
 
